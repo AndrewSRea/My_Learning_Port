4ec386046f5960afc8a7bc60a2acdb30e12c6ca1
AndrewSRea <andrew.s.rea@gmail.com>
2021-04-03 09:32:35 -0700

    Adding variables to contain strings to be used for Story Generator

--- JavaScript/JS_First_Steps/Assessment/main.js
+++ JavaScript/JS_First_Steps/Assessment/main.js
@@ -1,1 +1,24 @@
 // Adding a JS file to accompany the `index.html` file
+
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
+
+let insertX = ['Willy the Goblin',
+               'Big Daddy',
+               'Father Christmas'];
+
+let insertY = ['the soup kitchen',
+               'Disneyland',
+               'the White House'];
+
+let insertZ = ['spontaneously combusted',
+               'melted into a puddle on the sidewalk',
+               'turned into a slug and crawled away'];
+
+function randomValueFromArray(array){
+    const random = Math.floor(Math.random()*array.length);
+    return array[random];
+}
