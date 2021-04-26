dd1892ae1cd148b0064cbf13fa62015733157a90
AndrewSRea <andrew.s.rea@gmail.com>
2021-04-26 14:31:04 -0700

    Solution to first Assessment code block added

--- JavaScript/JS_Building_Blocks/Assessment/main.js
+++ JavaScript/JS_Building_Blocks/Assessment/main.js
@@ -1,13 +1,15 @@
 const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
+const thumbBar = document.querySelector('.thumb-bar');  // stores a reference to the `thumb-bar` <div> inside a constant named `thumbBar`
 
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
+for (i = 1; i <= 5; i++) {
+    const newImage = document.createElement('img'); // creates a new <img> element and stores it inside a constant named `newImage`
+    newImage.setAttribute('src', 'images/pic' + [i] + '.jpg'); // sets the `src` attribute of the `newImage` element to a placeholder value `xxx`
+    thumbBar.appendChild(newImage); // appends this `newImage` element inside the `thumbBar`
+}
 
 /* Wiring up the Darken/Lighten button */
