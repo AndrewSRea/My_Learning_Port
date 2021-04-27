d81d861641d962506f0affac716304f4b73adb58
AndrewSRea <andrew.s.rea@gmail.com>
2021-04-27 09:50:20 -0700

    Corrections and comments added to the final code

--- JavaScript/JS_Building_Blocks/Assessment/main.js
+++ JavaScript/JS_Building_Blocks/Assessment/main.js
@@ -7,20 +7,24 @@ const overlay = document.querySelector('.overlay');
 /* Looping through images */
 
 for (i = 1; i <= 5; i++) {
-    const newImage = document.createElement('img');
-    newImage.setAttribute('src', 'images/pic' + [i] + '.jpg');
+    let newImage = document.createElement('img');
+    newImage.setAttribute('src', 'images/pic' + i + '.jpg');
     thumbBar.appendChild(newImage); 
 
-    newImage.onclick = function() {
-        const bigImage = document.querySelector('.displayed-img');
-        bigImage.setAttribute(newImage.getAttribute());
+    newImage.onclick = function(e) {                                  // Correction on this line: an event (e) was added to the function
+        // let bigImage = document.querySelector('.displayed-img');
+        let imgAttr = e.target.getAttribute('src');                   // Explanation: `target`s the `newImage` `src` attribute and stores it in the `imgAttr` variable
+        // bigImage.setAttribute(newImage.getAttribute());
+        displayedImage.setAttribute('src', imgAttr);                  // Then sets the `src` attribute of the `displayedImage` <div> with the `newImage` `src` attribute (`imgAttr`)
     };
 }
 
 /* Wiring up the Darken/Lighten button */
 
 btn.onclick = function() {
-    if (btn.getAttribute() === 'dark') {
+    let btnAttr = btn.getAttribute('class');   // stores the `btn`s `class` attribute in a variable called `btnAttr`
+    // if (btn.getAttribute() === 'dark') {
+    if (btnAttr === 'dark') {
         btn.setAttribute('class', 'light');
         btn.textContent = 'Lighten';
         overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
