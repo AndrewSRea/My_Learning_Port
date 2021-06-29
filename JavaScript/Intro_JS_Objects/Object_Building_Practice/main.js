46d40e88e16400b6375cdac320311dc7032624c7
AndrewSRea <andrew.s.rea@gmail.com>
2021-06-29 15:50:05 -0700

    Creating a 'Ball' object constructor

--- JavaScript/Intro_JS_Objects/Object_Building_Practice/main.js
+++ JavaScript/Intro_JS_Objects/Object_Building_Practice/main.js
@@ -12,3 +12,12 @@ function random(min, max) {
     const num = Math.floor(Math.random() * (max - min + 1)) + min;
     return num;
 }
+
+function Ball(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+}
