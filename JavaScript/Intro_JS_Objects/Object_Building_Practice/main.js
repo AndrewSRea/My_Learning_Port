13ead19d82b285134b6cb15da36485e89d96495e
AndrewSRea <andrew.s.rea@gmail.com>
2021-07-01 16:01:05 -0700

    Adding a line in the animation 'loop()' which calls the 'collisionDetect()' function

--- JavaScript/Intro_JS_Objects/Object_Building_Practice/main.js
+++ JavaScript/Intro_JS_Objects/Object_Building_Practice/main.js
@@ -90,6 +90,7 @@ function loop() {
     for (let i = 0; i < balls.length; i++) {
         balls[i].draw();
         balls[i].update();
+        balls[i].collisionDetect();
     }
 
     requestAnimationFrame(loop);
