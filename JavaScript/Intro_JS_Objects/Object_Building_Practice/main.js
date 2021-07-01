a7cec71f7da04562d232ec12cadc8452b70db56f
AndrewSRea <andrew.s.rea@gmail.com>
2021-07-01 15:19:58 -0700

    Adding a function to detect collision amongst the Ball objects - when the Ball objects touch each other

--- JavaScript/Intro_JS_Objects/Object_Building_Practice/main.js
+++ JavaScript/Intro_JS_Objects/Object_Building_Practice/main.js
@@ -50,6 +50,20 @@ Ball.prototype.update = function() {
     this.y += this.velY;
 }
 
+Ball.prototype.collisionDetect = function() {
+    for (let j = 0; j < balls.length; j++) {
+        if (!(this === balls[j])) {
+            const dx = this.x - balls[j].x;
+            const dy = this.y - balls[j].y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+
+            if (distance < this.size + balls[j].size) {
+                balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) +')';
+            }
+        }
+    }
+}
+
 let balls = [];
 
 while (balls.length < 25) {
