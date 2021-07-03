2c979611e69f0bef9b45c84d5dc74c9be1363741
AndrewSRea <andrew.s.rea@gmail.com>
2021-07-03 14:52:13 -0700

    Must start JS code from scratch - read commit on README

--- JavaScript/Intro_JS_Objects/Assessment/main.js
+++ JavaScript/Intro_JS_Objects/Assessment/main.js
@@ -89,6 +89,8 @@ function loop() {
     ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
     ctx.fillRect(0, 0, width, height);
 
+    let evilCircle = new EvilCircle()
+
     for (let i = 0; i < balls.length; i++) {
         balls[i].draw();
         balls[i].update();
