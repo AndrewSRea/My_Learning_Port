80a69f76ed12d2a98a6dfde348675076b6caf5e4
AndrewSRea <andrew.s.rea@gmail.com>
2021-07-04 14:54:23 -0700

    Adding code to the 'loop()' function to add the 'EvilCircle' onto the screen, and creating a ball count incrementor/decrementor

--- JavaScript/Intro_JS_Objects/Assessment/main.js
+++ JavaScript/Intro_JS_Objects/Assessment/main.js
@@ -6,6 +6,9 @@ const ctx = canvas.getContext('2d');
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
 
+let para = document.querySelector('p');
+let ballCount = 0;
+
 // function to generate random number
 
 function random(min, max) {
@@ -110,10 +113,23 @@ function loop() {
     ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
     ctx.fillRect(0, 0, width, height);
 
+    let evilCircle = new EvilCircle(
+        random(0 + size,width - size),
+        random(0 + size,height - size),
+        true
+    )
+
+    evilCircle.setControls();
+
     for (let i = 0; i < balls.length; i++) {
-        balls[i].draw();
-        balls[i].update();
-        balls[i].collisionDetect();
+        balls[i].exists.draw();
+        balls[i].exists.update();
+        balls[i].exists.collisionDetect();
+        evilCircle.draw();
+        evilCircle.checkBounds();
+        evilCircle.collisionDetect();
+        ballCount++;
+        para.textContent += ballCount;
     }
 
     requestAnimationFrame(loop);
@@ -201,6 +217,8 @@ EvilCircle.prototype.collisionDetect = function() {
 
             if (distance < this.size + balls[j].size) {
                 balls[j].exists = false;
+                ballCount--;
+                para.textContent += ballCount;
             }
         }
     }
