2aab94d19d255d11263020967d3dd4dba6e3b9ee
AndrewSRea <andrew.s.rea@gmail.com>
2021-08-08 09:39:06 -0700

    Saving JS code entered thus far - creating Three.js 'Scene', 'Camera', and 'Renderer' objects

---
+++ JavaScript/Client-side_Web_APIs/Drawing_Graphics/WebGL_Graphic/main.js
@@ -0,0 +1,6 @@
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
