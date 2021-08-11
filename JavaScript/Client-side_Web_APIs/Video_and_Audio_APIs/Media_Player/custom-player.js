6db44188b501213c5fd31ad0d418694465031a34
AndrewSRea <andrew.s.rea@gmail.com>
2021-08-11 10:51:40 -0700

    Adding a JS file to manipulate the video player controls in the accompanying 'index.html' file

---
+++ JavaScript/Client-side_Web_APIs/Video_and_Audio_APIs/Media_Player/custom-player.js
@@ -0,0 +1,11 @@
+const media = document.querySelector('video');
+const controls = document.querySelector('.controls');
+
+const play = document.querySelector('.play');
+const stop = document.querySelector('.stop');
+const rwd = document.querySelector('.rwd');
+const fwd = document.querySelector('.fwd');
+
+const timerWrapper = document.querySelector('.timer');
+const timer = document.querySelector('.timer span');
+const timerBar = document.querySelector('.timer div');
