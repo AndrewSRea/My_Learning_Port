695a74bb4aa701d4b99150c21d93882af4e18ca8
AndrewSRea <andrew.s.rea@gmail.com>
2021-08-11 11:05:07 -0700

    Adding a code block to manipulate the 'play/pause' button to toggle between 'play' and 'paused' states

--- JavaScript/Client-side_Web_APIs/Video_and_Audio_APIs/Media_Player/custom-player.js
+++ JavaScript/Client-side_Web_APIs/Video_and_Audio_APIs/Media_Player/custom-player.js
@@ -9,3 +9,18 @@ const fwd = document.querySelector('.fwd');
 const timerWrapper = document.querySelector('.timer');
 const timer = document.querySelector('.timer span');
 const timerBar = document.querySelector('.timer div');
+
+media.removeAttribute('controls');
+controls.style.visibility = 'visible';
+
+play.addEventListener('click', playPauseMedia);
+
+function playPauseMedia() {
+    if(media.paused) {
+        play.setAttribute('data-icon', 'u');
+        media.play();
+    } else {
+        play.setAttribute('data-icon', 'P');
+        media.pause();
+    }
+}
