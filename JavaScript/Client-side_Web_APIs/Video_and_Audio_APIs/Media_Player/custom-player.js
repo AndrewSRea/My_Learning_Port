53a01c4c1da43565db84458cf65dab8e2c5dcf97
AndrewSRea <andrew.s.rea@gmail.com>
2021-08-12 09:03:00 -0700

    Adding a function to set and display the elapsed time on the video player

--- JavaScript/Client-side_Web_APIs/Video_and_Audio_APIs/Media_Player/custom-player.js
+++ JavaScript/Client-side_Web_APIs/Video_and_Audio_APIs/Media_Player/custom-player.js
@@ -18,6 +18,7 @@ stop.addEventListener('click', stopMedia);
 media.addEventListener('ended', stopMedia);
 rwd.addEventListener('click', mediaBackward);
 fwd.addEventListener('click', mediaForward);
+media.addEventListener('timeupdate', setTime);
 
 function playPauseMedia() {
     if(media.paused) {
@@ -53,4 +54,29 @@ function windForward() {
     } else {
         media.currentTime += 3;
     }
+}
+
+function setTime() {
+    let minutes = Math.floor(media.currentTime / 60);
+    let seconds = Math.floor(media.currentTime - minutes * 60);
+    let minuteValue;
+    let secondValue;
+
+    if (minutes < 10) {
+        minuteValue = '0' + minutes;
+    } else {
+        minuteValue = minutes;
+    }
+
+    if (seconds < 10) {
+        secondValue = '0' + seconds;
+    } else {
+        secondValue = seconds;
+    }
+
+    let mediaTime = minuteValue + ':' + secondValue;
+    timer.textContent = mediaTime;
+
+    let barLength = timerWrapper.clientWidth * (media.currentTime/media.duration);
+    timerBar.style.width = barLength + 'px';
 }
