39ac860e7dac4e3f996b5cb23f8f22ad7f276210
AndrewSRea <andrew.s.rea@gmail.com>
2021-08-13 15:14:27 -0700

    All function code blocks added

--- JavaScript/Client-side_Web_APIs/Client-side_Storage/index.js
+++ JavaScript/Client-side_Web_APIs/Client-side_Storage/index.js
@@ -8,3 +8,49 @@ const forgetBtn = document.querySelector('#forgetname');
 
 const h1 = document.querySelector('h1');
 const personalGreeting = document.querySelector('.personal-greeting');
+
+// Stop the form from submitting when a button is pressed
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+});
+
+// run function when the 'Say hello' button is clicked
+submitBtn.addEventListener('click', function() {
+    // store the entered name in web storage
+    localStorage.setItem('name', nameInput.value);
+    // run nameDisplayCheck() to sort out displaying the
+    // personalized greetings and updating the form display
+    nameDisplayCheck();
+});
+
+// run function when the 'Forget' button is clicked
+forgetBtn.addEventListener('click', function() {
+    // Remove the stored name from web storage
+    localStorage.removeItem('name');
+    // run nameDisplayCheck() to sort out displaying the
+    // generic greeting again and updating the form display
+    nameDisplayCheck();
+});
+
+// define the nameDisplayCheck() function
+function nameDisplayCheck() {
+    // check whether the 'name' data item is stored in web Storage
+    if(localStorage.getItem('name')) {
+        // If it is, display personalized greeting
+        let name = localStorage.getItem('name');
+        h1.textContent = 'Welcome, ' + name;
+        personalGreeting.textContent = 'Welcome to our website, ' + name + '! We hope you have fun while you are here.';
+        // hide the 'remember' part of the form and show the 'forget' part
+        forgetDiv.style.display = 'block';
+        rememberDiv.style.display ='none';
+    } else {
+        // if not, display generic greeting
+        h1.textContent ='Welcome to our website ';
+        personalGreeting.textContent = 'Welcome to our website. We hope you have fun while you are here.';
+        // hide the 'forget' part of the form and show the 'remember' part
+        forgetDiv.style.display = 'none';
+        rememberDiv.style.display = 'block';
+    }
+}
+
+document.body.onload = nameDisplayCheck;
