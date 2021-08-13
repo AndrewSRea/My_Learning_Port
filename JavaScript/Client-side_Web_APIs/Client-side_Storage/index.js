3d04557d405d161fb0528e1c72d9ba9483dd3e83
AndrewSRea <andrew.s.rea@gmail.com>
2021-08-13 14:32:31 -0700

    Adding a JS script to accompany the 'personal-greeting.html' file, and adding some constants

---
+++ JavaScript/Client-side_Web_APIs/Client-side_Storage/index.js
@@ -0,0 +1,10 @@
+// create needed constants
+const rememberDiv = document.querySelector('.remember');
+const forgetDiv = document.querySelector('.forget');
+const form = document.querySelector('form');
+const nameInput = document.querySelector('#entername');
+const submitBtn = document.querySelector('#submitname');
+const forgetBtn = document.querySelector('#forgetname');
+
+const h1 = document.querySelector('h1');
+const personalGreeting = document.querySelector('.personal-greeting');
