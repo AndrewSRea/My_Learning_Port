985a66b8a5ce00c879009dda0a58ae18101e10f6
AndrewSRea <andrew.s.rea@gmail.com>
2021-08-15 10:20:31 -0700

    Adding code blocks to create a database variable, a 'request' to the database, and event handlers for the success or failure of the request

--- JavaScript/Client-side_Web_APIs/Client-side_Storage/IndexedDB_App/index.js
+++ JavaScript/Client-side_Web_APIs/Client-side_Storage/IndexedDB_App/index.js
@@ -4,3 +4,12 @@ const titleInput = document.querySelector('#title');
 const bodyInput = document.querySelector('#body');
 const form = document.querySelector('form');
 const submitBtn = document.querySelector('form button');
+
+// Create an instance of a db object for us to store the open database in
+let db;
+
+window.onload = function() {
+    // Open our database; it is created if it doesn't already exist
+    // (see onupgradeneeded below)
+    let request = window.indexedDB.open('notes_db', 1);
+};
