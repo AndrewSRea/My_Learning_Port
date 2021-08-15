0f9e1c13b99316a5d1ce4776160d9cdb96723be2
AndrewSRea <andrew.s.rea@gmail.com>
2021-08-15 11:16:13 -0700

    Adding a code block for an 'onupgradeneeded' request event handler to create a schema from the IndexedDB

--- JavaScript/Client-side_Web_APIs/Client-side_Storage/IndexedDB_App/index.js
+++ JavaScript/Client-side_Web_APIs/Client-side_Storage/IndexedDB_App/index.js
@@ -12,4 +12,36 @@ window.onload = function() {
     // Open our database; it is created if it doesn't already exist
     // (see onupgradeneeded below)
     let request = window.indexedDB.open('notes_db', 1);
+
+    // onerror handler signifies that the database didn't open successfully
+    request.onerror = function() {
+        console.log('Database failed to open');
+    };
+
+    // onsuccess handler signifies that the database opened successfully
+    request.onsuccess = function() {
+        console.log('Database opened successfully');
+
+        // Store the opened database object in the db variable. This is used a lot below
+        db = request.result;
+
+        // Run the displayData() function to display the notes already in the IDB
+        displayData();
+    };
+
+    // Setup the database tables if this has not already been done
+    request.onupgradeneeded = function(e) {
+        // Grab a reference to the opened database
+        let db = e.target.result;
+
+        // Create an objectStore to store our notes in (basically like a single table)
+        // including an auto-incrementing key
+        let objectStore = db.createObjectStore('notes_os', { keyPath: 'id', autoIncrement:true });
+
+        // Define what data items the objectStore will contain
+        objectStore.createIndex('title', 'title', { unique: false });
+        objectStore.createIndex('body', 'body', { unique: false });
+
+        console.log('Database setup complete');
+    };
 };
